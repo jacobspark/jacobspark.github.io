161dccece163d2ed7643b00bcf40e3b78741757a
Jacob <soobin.park@vanderbilt.edu>
2020-07-13 19:02:26 -0700

    Modified animation on hr

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 $("h1").delay(500).animate({"opacity": "1"}, 2000);
+$("hr").delay(1000).animate({"opacity": "1"}, 1000);
 $("h5").delay(1500).animate({"opacity": "1"}, 2000);
 
 $("#icon1").delay(2500).animate({"opacity": "1"}, 500);
